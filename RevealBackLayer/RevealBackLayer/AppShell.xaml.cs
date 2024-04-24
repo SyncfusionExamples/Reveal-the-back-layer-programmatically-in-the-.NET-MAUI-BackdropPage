899043a65c7cbfdeb4cccc5b843bc17940c64967
VishalOmprasad <102582824+VishalOmprasad@users.noreply.github.com>
2024-04-24 18:59:46 +0530

    New KB sample (with Behavior) added

---
+++ RevealBackLayer/RevealBackLayer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RevealBackLayer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RevealBackLayer/RevealBackLayer/Behavior/BackdropPageBehavior.cs
@@ -0,0 +1,51 @@
+﻿using Syncfusion.Maui.Backdrop;
+
+namespace RevealBackLayer
+{
+    public class BackdropPageBehavior : Behavior<SfBackdropPage>
+    {
+        private SfBackdropPage? backdropPage;
+        private Button? revealButton;
+        private bool IsBackLayerRevealed;
+
+        protected override void OnAttachedTo(SfBackdropPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.backdropPage = bindable.FindByName<SfBackdropPage>("backdropPage");
+            this.revealButton = bindable.FindByName<Button>("revealButton");
+            this.revealButton.Clicked += RevealButton_Clicked;
+        }
+
+        private void RevealButton_Clicked(object? sender, EventArgs e)
+        {
+            if (this.backdropPage != null)
+            {
+                if (!IsBackLayerRevealed)
+                {
+                    this.IsBackLayerRevealed = true;
+                    this.backdropPage.IsBackLayerRevealed = this.IsBackLayerRevealed;
+                }
+                else
+                {
+                    this.IsBackLayerRevealed = false;
+                    this.backdropPage.IsBackLayerRevealed = this.IsBackLayerRevealed;
+                }
+            }
+        }
+
+        protected override void OnDetachingFrom(SfBackdropPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            if (this.backdropPage != null)
+            {
+                this.backdropPage = null;
+            }
+            
+            if (this.revealButton != null)
+            {
+                this.revealButton.Clicked -= RevealButton_Clicked;
+                this.revealButton = null;
+            }
+        }
+    }
+}
